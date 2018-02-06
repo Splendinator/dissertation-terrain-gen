4e3706f2c1c4237d1494bb5e9a5bb0c35358eb1f
Dominic <dominicburns97@outlook.com>
2018-02-06 16:31:36 +0000

    Bug fixes and serious optimisation

--- Programs/TerrainGenV1/Renderer.h
+++ Programs/TerrainGenV1/Renderer.h
@@ -12,7 +12,7 @@ class Renderer : public OGLRenderer {
 private:
 	static const int MAX_CHUNKS = 19;	//The maximum ammount of chunks that will be rendered every frame. (3 means 3x3 grid around player)
 	static const int MAX_THREADS = 7;	//The number of threads that we can use. More = faster terrain generation but more CPU usage.
-	int cameraPosX = MAX_CHUNKS / 2, cameraPosY = MAX_CHUNKS / 2;
+	int cameraPosX = MAX_CHUNKS / 2, cameraPosY = MAX_CHUNKS / 2; // Chunks that the camera is above.
 	
 	
 	Chunk *chunk[MAX_CHUNKS*MAX_CHUNKS]; //array of pointers to all the chunks;
